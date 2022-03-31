ebe28753bd2dc1208afb330503557174ac87ac55
xsahxl <xsahxl@126.com>
2022-03-31 14:12:45 +0800

    fix: 🐛 core esbuild cannot require package.json

--- src/util/helper.ts
+++ src/util/helper.ts
@@ -1,10 +1,12 @@
-import * as os from 'os';
-import kitx from 'kitx';
-import path from 'path';
+import * as os from "os";
 
-const pkg = kitx.loadJSONSync(path.join(__dirname, '../../package.json'));
+const pkg = {
+  name: "@alicloud/credentials",
+  version: "2.2.1",
+};
 
-export const DEFAULT_UA = `AlibabaCloud (${os.platform()}; ${os.arch()}) ` +
-    `Node.js/${process.version} Core/${pkg.version}`;
+export const DEFAULT_UA =
+  `AlibabaCloud (${os.platform()}; ${os.arch()}) ` +
+  `Node.js/${process.version} Core/${pkg.version}`;
 
 export const DEFAULT_CLIENT = `Node.js(${process.version}), ${pkg.name}: ${pkg.version}`;
